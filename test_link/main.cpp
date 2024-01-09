0c785c88e54f2a7d8e8af040c9c223a87a293bbf
nicolas le goff <nicolas.le-goff@cea.fr>
2024-01-09 11:35:12 +0100

    fix test_link

--- test_link/main.cpp
+++ test_link/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
-#include <smooth3D/smooth3D.h>
+
+#include <smooth3D/smooth3d.h>
+// what to do with machine_types ?
+//#include <machine_types.h>
 
 int main() {
 
-    S3_CELL_3D cell;
-    cell.nb_nodes = 8;
-    cell.vtx[0];
+/*
+    int_type nb_cells = 1;
+    int_type nb_nodes = 4;
+    int_type nb_nodes_per_cell[1] = {4};
+    int_type nodes_number[4] = {0,1,2,3}
+    double x[4] = {0., 1., 1., 0.};
+    double y[4] = {0., 0., 1., 1.};
+    double z[4] = {0., 0., 0., 0.};
+    double weights[4] = {1., 1., 1., 1.};
+    double relax[4] = {1., 1., 1., 1.};
+    int_type n_iter = 2;
+    
+    int ret = S3_laplace(nb_cells, nb_nodes, nb_nodes_per_cell, nodes_number, x, y, z, weights, relax, n_iter);
+*/
     return 0;
 }
